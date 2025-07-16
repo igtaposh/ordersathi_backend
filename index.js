edd61901264cfae14aa91ed1073be925f84f6443
igtaposh <debnathtaposh58@gamil>
2025-07-16 17:55:20 +0530

    cors error

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(ccokiesParser());
-app.use(cors());
+app.use(cors({
+   origin: 'http://localhost:5173', // or your frontend URL
+   credentials: true
+}));
 
 // Routes
 app.use('/api/auth', authRoutes);
